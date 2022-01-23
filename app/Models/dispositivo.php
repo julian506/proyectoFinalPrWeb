b9a7c83ea90f88b7ab6a155157daaf745777aa56
PaulaTaborda7 <ptabordam@unal.edu.co>
2022-01-23 18:21:55 -0500

    Se especificó llave primaria en el modelo de la tabla dispositivos

--- app/Models/dispositivo.php
+++ app/Models/dispositivo.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class dispositivo extends Model
+class Dispositivo extends Model
 {
     use HasFactory;
+    protected $table = 'dispositivos';
+    protected $primaryKey = "id";
 }
