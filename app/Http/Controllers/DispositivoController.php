ace29f67d5d6e22311a000b4e7dc8f7d1aeb2f67
PaulaTaborda7 <ptabordam@unal.edu.co>
2022-01-23 14:40:43 -0500

    Modelo controlador y migración de dispositivo

---
+++ app/Http/Controllers/DispositivoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\dispositivo;
+use Illuminate\Http\Request;
+
+class DispositivoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\dispositivo  $dispositivo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(dispositivo $dispositivo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\dispositivo  $dispositivo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(dispositivo $dispositivo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\dispositivo  $dispositivo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, dispositivo $dispositivo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\dispositivo  $dispositivo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(dispositivo $dispositivo)
+    {
+        //
+    }
+}
