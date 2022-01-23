f70bb6837a64f1622db11f16e51aae8b45168f97
PaulaTaborda7 <ptabordam@unal.edu.co>
2022-01-23 18:21:27 -0500

    Se dieron las especificaciones de cada método para el CRUD de dispositivos

--- app/Http/Controllers/DispositivoController.php
+++ app/Http/Controllers/DispositivoController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\dispositivo;
+use App\Http\Controllers\Controller;
+use App\Models\Dispositivo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class DispositivoController extends Controller
 {
@@ -14,7 +16,9 @@ class DispositivoController extends Controller
      */
     public function index()
     {
-        //
+        //Para traer todos los productos de la BD:
+        $dispositivos = Dispositivo::all();
+        return view('admin/dispositivos.index')->with('dispositivos', $dispositivos);
     }
 
     /**
@@ -24,7 +28,8 @@ class DispositivoController extends Controller
      */
     public function create()
     {
-        //
+        //Sirve para redireccionarme a la vista
+        return view("admin/dispositivos.guardar");
     }
 
     /**
@@ -35,16 +40,25 @@ class DispositivoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $dispositivo = new Dispositivo();
+        $dispositivo->nombre = $request->nombre;
+        $dispositivo->descripcion = $request->descripcion;
+        $dispositivo->precio = $request->precio;
+        if($request->hasFile('imagen')){
+            $dispositivo->imagen = $request->file('imagen')->store('uploads','public');
+        }
+        $dispositivo->save();
+
+        return redirect()->route('dispositivos.index');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\dispositivo  $dispositivo
+     * @param  \App\Models\Dispositivo  $dispositivo
      * @return \Illuminate\Http\Response
      */
-    public function show(dispositivo $dispositivo)
+    public function show(Dispositivo $dispositivo)
     {
         //
     }
@@ -52,34 +66,57 @@ class DispositivoController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\dispositivo  $dispositivo
+     * @param  \App\Models\Dispositivo  $dispositivo
      * @return \Illuminate\Http\Response
      */
-    public function edit(dispositivo $dispositivo)
+    public function edit($id)
     {
-        //
+        $dispositivo = Dispositivo::find($id);
+        return view('admin/dispositivos.editar')->with('dispositivo',$dispositivo);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\dispositivo  $dispositivo
+     * @param  \App\Models\Dispositivo  $dispositivo
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, dispositivo $dispositivo)
+    public function update(Request $request, $id)
     {
-        //
+        $validated = $request->validate([
+            'nombre' => 'required',
+            'descripcion' => 'required',
+            'precio' => 'required',
+        ]);
+
+        $dispositivo = Dispositivo::find($id);
+
+        $dispositivo->nombre = $request->nombre;
+        $dispositivo->descripcion = $request->descripcion;
+        $dispositivo->precio = $request->precio;
+        //Si en el formulario se adjuntó una NUEVA foto...
+        if($request->hasFile('imagen')){
+            //Borramos de la carpeta public aquella imagen que tenga la misma ruta que $empleado->foto
+            Storage::delete('public/'.$dispositivo->imagen);
+            //Suba la NUEVA foto a la carpeta uploads en public y en el JSON guarde la dirección de la foto
+            $dispositivo->imagen = $request->file('imagen')->store('uploads', 'public');
+        }
+        $dispositivo->save();
+
+        return redirect()->route('dispositivos.index');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\dispositivo  $dispositivo
+     * @param  \App\Models\Dispositivo  $dispositivo
      * @return \Illuminate\Http\Response
      */
-    public function destroy(dispositivo $dispositivo)
+    public function destroy($id)
     {
-        //
+        $dispositivo = Dispositivo::where('id', $id);
+        $dispositivo->delete();
+        return redirect()->route('dispositivos.index');
     }
 }
