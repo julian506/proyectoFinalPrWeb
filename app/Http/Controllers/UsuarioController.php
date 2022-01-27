e62f5894b76849de76f48ae49311550badf7f2a2
jupachon <jupachon@unal.edu.co>
2022-01-27 12:13:57 -0500

    Cambio la paginación del catálogo a 6 productos

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class UsuarioController extends Controller
 {
     function usuarioIndex(){
-        $dispositivos = Dispositivo::where('cantidad', '>', 0)->paginate(9);
+        $dispositivos = Dispositivo::where('cantidad', '>', 0)->paginate(6);
         return view('usuario.index', compact('dispositivos'));
     }
 }
