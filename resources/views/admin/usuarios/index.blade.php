7d8bd70048d8e6acc5777679bc09ddd284d0fdf3
jupachon <jupachon@unal.edu.co>
2022-01-30 13:34:36 -0500

    Corrijo vista de crud de usuarios

--- resources/views/admin/usuarios/index.blade.php
+++ resources/views/admin/usuarios/index.blade.php
@@ -1,60 +1,70 @@
 @extends('layouts.appAdmin')
-<div class="container text-center">
-    <a href="{{ route("admin.usuarios.crear") }}">
-        <button class="btn btn-success">
-            Crear
-        </button>
-    </a>
-    <table id="example" class="table table-striped" style="width:100%">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Nombre</th>
-                <th>Apellidos</th>
-                <th>Correo</th>
-                <td>Opciones</td>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($usuarios as $usuario)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $usuario->nombre }}</td>
-                    <td>{{ $usuario->apellidos }}</td>
-                    <td>{{ $usuario->correo }}</td>
-                    <td>
-                        <a href="{{ route('usuarios.edit', $usuario->id) }}">
-                            <button class="btn btn-primary">
-                                Editar
-                            </button>
-                        </a>
-                        {{-- Formulario de borrado --}}
-                        <form  action="{{route('admin.usuarios.destroy',$usuario->id)}}" method="post">
-                            @method('DELETE')
-                            @csrf
-                            <input class="btn btn-danger" type="submit" onclick="return confirm('¿Quieres borrar?')" value="Borrar">
-                        </form>
+@section('content')
+	<div class="container text-center">
+		<div class="card">
+			<div class="card-header">
+				<h3 class="text-center">CRUD de usuarios</h3>
+			</div>
+			<div class="card-body">
+				<a href="{{ route('admin.usuarios.crear') }}">
+					<button class="btn btn-success">
+						Crear
+					</button>
+				</a>
+				<table id="example" class="table table-striped" style="width:100%">
+					<thead>
+						<tr>
+							<th>Id</th>
+							<th>Nombre</th>
+							<th>Apellidos</th>
+							<th>Correo</th>
+							<td>Opciones</td>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach ($usuarios as $usuario)
+							<tr>
+								<td>{{ $loop->iteration }}</td>
+								<td>{{ $usuario->nombre }}</td>
+								<td>{{ $usuario->apellidos }}</td>
+								<td>{{ $usuario->correo }}</td>
+								<td>
+									<a href="{{ route('usuarios.edit', $usuario->id) }}">
+										<button class="btn btn-primary">
+											Editar
+										</button>
+									</a>
+									{{-- Formulario de borrado --}}
+									<form action="{{ route('admin.usuarios.destroy', $usuario->id) }}" method="post">
+										@method('DELETE')
+										@csrf
+										<input class="btn btn-danger" type="submit" onclick="return confirm('¿Quieres borrar?')" value="Borrar">
+									</form>
 
-                    </td>
+								</td>
 
-                </tr>
-            @endforeach
-        </tbody>
-        <tfoot>
-            <tr>
-                <th>Id</th>
-                <th>Nombre</th>
-                <th>Apellidos</th>
-                <th>Correo</th>
-            </tr>
-        </tfoot>
-    </table>
-</div>
-<script>
-    $(document).ready(function () {
-        var table = $('#example').DataTable({
-            responsive: true
-        });
-        new $.fn.dataTable.FixedHeader(table);
-    });
-</script>
+							</tr>
+						@endforeach
+					</tbody>
+					<tfoot>
+						<tr>
+							<th>Id</th>
+							<th>Nombre</th>
+							<th>Apellidos</th>
+							<th>Correo</th>
+							<th>Opciones</th>
+						</tr>
+					</tfoot>
+				</table>
+			</div>
+		</div>
+	</div>
+	<script>
+	 $(document).ready(function() {
+	  var table = $('#example').DataTable({
+	   responsive: true
+	  });
+	  new $.fn.dataTable.FixedHeader(table);
+	 });
+	</script>
+@endsection
