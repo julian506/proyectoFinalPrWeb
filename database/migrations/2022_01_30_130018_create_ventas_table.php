67b44bf5f3d220fdd778397ed3bfc440883798ea
PaulaTaborda7 <ptabordam@unal.edu.co>
2022-01-30 20:21:50 -0500

    Se crea la migración de ventas

---
+++ database/migrations/2022_01_30_130018_create_ventas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVentasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('idUsuario');
+            $table->unsignedBigInteger('idDispositivo');
+            $table->integer('cantidad');
+            $table->foreign('idUsuario')->references('id')->on('usuarios');
+            $table->foreign('idDispositivo')->references('id')->on('dispositivos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ventas');
+    }
+}
